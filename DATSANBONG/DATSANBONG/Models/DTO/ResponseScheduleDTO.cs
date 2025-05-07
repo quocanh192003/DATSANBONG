8474d6f27f9dfe0d52720ad2ff7b699d0d6c4db9
Ho Dang Quoc Anh <hodangquocanh2003@gmail.com>
2025-05-07 17:47:19 +0700

    feat: CRUD Schedule

---
+++ DATSANBONG/DATSANBONG/Models/DTO/ResponseScheduleDTO.cs
@@ -0,0 +1,14 @@
+﻿namespace DATSANBONG.Models.DTO
+{
+    public class ResponseScheduleDTO
+    {
+        public string MaLichSan { get; set; }
+        public string MaSanCon { get; set; }
+        public string MaSanBong { get; set; }
+        public string thu { get; set; }
+        public string GioBatDau { get; set; }
+        public string GioKetThuc { get; set; }
+        public decimal GiaThue { get; set; }
+        public string TrangThai { get; set; }
+    }
+}
---
+++ DATSANBONG/DATSANBONG/Repository/IRepository/IScheduleRepository.cs
@@ -0,0 +1,14 @@
+﻿using DATSANBONG.Models;
+using DATSANBONG.Models.DTO;
+
+namespace DATSANBONG.Repository.IRepository
+{
+    public interface IScheduleRepository
+    {
+        Task<APIResponse> CreatSchedule(LichSanDTO request);
+        Task<APIResponse> GetAllSchedule();
+        Task<APIResponse> GetScheduleByIdDetailFootball(string id);
+        Task<APIResponse> UpdateSchedule(string id, UpdateScheduleDTO request);
+        Task<APIResponse> DeleteSchedule(string id);
+    }
+}
