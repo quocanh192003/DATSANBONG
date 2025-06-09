d76adc5568083434381a0df5ad43e27aa4b6f141
Ho Dang Quoc Anh <hodangquocanh2003@gmail.com>
2025-06-09 22:25:24 +0700

    update apiresponse

--- DATSANBONG/DATSANBONG/Models/APIResponse.cs
+++ DATSANBONG/DATSANBONG/Models/APIResponse.cs
@@ -4,13 +4,23 @@ namespace DATSANBONG.Models
 {
     public class APIResponse
     {
+        private List<string> _errorMessages;
+
         public APIResponse()
         {
-            ErrorMessages = new List<string>();
+            _errorMessages = new List<string>();
         }
+
         public HttpStatusCode Status { get; set; }
+
         public bool IsSuccess { get; set; } = true;
-        public List<string> ErrorMessages { get; set; }
-        public Object Result { get; set; }
+
+        public List<string> ErrorMessages
+        {
+            get => _errorMessages ??= new List<string>();
+            set => _errorMessages = value ?? new List<string>();
+        }
+
+        public object Result { get; set; }
     }
 }
